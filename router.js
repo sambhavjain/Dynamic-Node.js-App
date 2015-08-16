9bdd7a7fb1745c2d316b4b9e5041d23a4077ca5c
sambhavjain <sambhav.jiit@gmail.com>
2015-08-16 15:40:11 +0530

    New router after introducing rendere.js

--- router.js
+++ router.js
@@ -1,15 +1,16 @@
 
 var Profile = require('./profile.js');
+var render = require('./renderer.js');
 // Handle a HTTP route GET / and POST /i.e Home
 function home(request, response) {
 	//if  url === "/" && GET
 	if(request.url === "/") {
 		//show search
-		response.writeHead(200,{'Content-Type': 'text/plain'});
-		response.write('Header\n');
-		response.write('Search\n');
-		response.end('Footer\n');
-
+		response.writeHead(200,{'Content-Type': 'text/html'});
+		render.view("header", {}, response);
+		render.view("search", {}, response);
+		render.view("footer", {}, response);
+		response.end();
 	}
 		//if url == "/" && POST
 			//redirect to /:username
@@ -19,8 +20,8 @@ function home(request, response) {
 	//if url == "/..."
 	var username = request.url.replace("/", "");
 	if(username.length > 0) {
-		response.writeHead(200,{'Content-Type': 'text/plain'});
-		response.write('Header\n');
+		response.writeHead(200,{'Content-Type': 'text/html'});
+		render.view("header", {}, response);
 		//get json from Treehouse
 		var studentProfile = new Profile(username);
 		//on "end"
@@ -32,14 +33,17 @@ function home(request, response) {
 				badgeCount : profileJSON.badges.length,
 				javascriptPoints : profileJSON.points.JavaScript
 			 }
-			response.write(values.username + " has " + values.badgeCount + " badges " + "\n");
-	 		response.end('Footer\n');
+			render.view("profile", values, response);
+	 		render.view("footer", {}, response);
+			response.end();
 		});
 		//on "error"
 		studentProfile.on("error", function(error){
 		//show error
-			response.write(error.message + "\n");
-			response.end('Footer\n');
+			render.view("error", {errorMessage:error.message}, response);
+			render.view("search", {}, response);
+			render.view("footer", {}, response);
+			response.end();
 		});
 
 	}
